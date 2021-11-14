4d91a4fcac06b8813b92b1e5b2c8f890e6d5d427
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-14 18:24:54 +0530

    feat: added solutions till question 9

---
+++ pages/build-questions/question3.jsx
@@ -0,0 +1,54 @@
+import Heading from '../../components/Heading'
+import Navbar from '../../components/Navbar'
+import { useState } from 'react'
+import QuestionContainer from '../../components/QuestionContainer'
+import QuestionHeader from '../../components/QuestionText'
+import styles from '../../styles/Questions.module.css'
+
+const questionText =
+  'Q.3. Create a web app where I can input a text. Now create two buttons + and -. On clicking + increase the fontSize by 2px and vice versa.'
+export default function () {
+  const [input, setInput] = useState('')
+  const [fontSize, setFontSize] = useState(16)
+
+  const handleClick = (e, instructions) => {
+    e.preventDefault()
+    instructions === '+' ? setFontSize(fontSize + 2) : setFontSize(fontSize - 2)
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Heading heading="Question 3" />
+      <QuestionHeader questionText={questionText} />
+      <QuestionContainer>
+        <label htmlFor="input">Input: </label>
+        <input
+          name="input"
+          type="text"
+          value={input}
+          placeholder="Enter your text here."
+          className={styles.inputStyles}
+          onChange={e => setInput(e.target.value)}
+        />
+        <br />
+        <div>
+          {['+', '-'].map((val, i) => (
+            <button
+              className={styles.calculateBtn}
+              key={i}
+              disabled={input ? false : true}
+              onClick={e => handleClick(e, val)}
+            >
+              <b>{val}</b>
+            </button>
+          ))}
+        </div>
+        <p>
+          Current font-size: <b>{fontSize}px</b>{' '}
+        </p>
+        <p style={{ fontSize: `${fontSize}px` }}>{input}</p>
+      </QuestionContainer>
+    </>
+  )
+}
---
+++ pages/build-questions/question9.jsx
@@ -0,0 +1,33 @@
+import Heading from '../../components/Heading'
+import Navbar from '../../components/Navbar'
+import { useState } from 'react'
+import QuestionContainer from '../../components/QuestionContainer'
+import QuestionHeader from '../../components/QuestionText'
+import styles from '../../styles/Questions.module.css'
+const questionText =
+  "8. Here's an API. It will give an error. Write a web app, call this API and read the error message. Show user the error message."
+
+const URL = 'https://quick-access-api.desaihetav.repl.co/'
+export default function () {
+  const [message, setMessage] = useState('')
+
+  const handleClick = async () => {
+    const res = await fetch(URL)
+    const data = await res.json()
+    console.log(data)
+    setMessage(data.message)
+  }
+  return (
+    <>
+      <Navbar />
+      <Heading heading="Question 9" />
+      <QuestionContainer>
+        <QuestionHeader questionText={questionText} />
+        <button className={styles.loadingTemplate} onClick={handleClick}>
+          Fetch
+        </button>
+        <h2>{message}</h2>
+      </QuestionContainer>
+    </>
+  )
+}
