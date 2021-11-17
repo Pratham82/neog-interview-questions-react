a9ba1aac01c30be6c193381eb0921b119d33a5e8
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-17 23:53:52 +0530

    feat: added mock questions list

--- pages/mock-questions/index.jsx
+++ pages/mock-questions/index.jsx
@@ -1,7 +1,10 @@
 import Navbar from '../../components/Navbar'
 import Head from 'next/head'
 import Heading from '../../components/Heading'
+import Link from 'next/link'
 import Container from '../../components/Container'
+import mockQuestions from '../../data/mockQuestions'
+import styles from '../../styles/Build.module.css'
 
 export default function MockQuestions() {
   return (
@@ -12,6 +15,18 @@ export default function MockQuestions() {
       <Navbar />
       <Container>
         <Heading heading="Mock Interview Questions" />
+        {mockQuestions.map(({ question, link }, i) => (
+          <Link href={link} key={i} passHref>
+            <div className={styles.questionContainer}>
+              <div>
+                {i + 1}. {question}
+              </div>
+              <div className={styles.linkContainer}>
+                {link !== '/mock-questions/' ? 'Live 🟢' : 'N.A. ⛔'}
+              </div>
+            </div>
+          </Link>
+        ))}
       </Container>
     </>
   )
