872e24e416141edb8f9ca4c111d387fdd18a96ae
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-17 23:54:07 +0530

    feat: added mock questions data

---
+++ data/mockQuestions.js
@@ -0,0 +1,56 @@
+const mockQuestions = [
+  {
+    question: 'Mock 1.1',
+    link: '/mock-questions/mock1-1',
+  },
+  {
+    question: 'Mock 1.2',
+    link: '/mock-questions/mock1-2',
+  },
+  {
+    question: 'Mock 2.1',
+    link: '/mock-questions/mock2-1',
+  },
+  {
+    question: 'Mock 2.2',
+    link: '/mock-questions/mock2-2',
+  },
+  {
+    question: 'Mock 3.1',
+    link: '/mock-questions/mock3-1',
+  },
+  {
+    question: 'Mock 3.2',
+    link: '/mock-questions/mock3-2',
+  },
+  {
+    question: 'Mock 3.3',
+    link: '/mock-questions/mock3-3',
+  },
+  {
+    question: 'Mock 4.1',
+    link: '/mock-questions/mock4-1',
+  },
+  {
+    question: 'Mock 4.2',
+    link: '/mock-questions/mock4-2',
+  },
+  {
+    question: 'Mock 5.1',
+    link: '/mock-questions/mock5-1',
+  },
+  {
+    question: 'Mock 5.2',
+    link: '/mock-questions/mock5-2',
+  },
+  {
+    question: 'Mock 6.1',
+    link: '/mock-questions/mock6-1',
+  },
+  {
+    question: 'Mock 6.2',
+    link: '/mock-questions/mock6-2',
+  },
+]
+
+export default mockQuestions
