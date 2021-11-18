1e71348bc5519d93965676b8755f41097e9e960c
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-18 23:00:22 +0530

    fix(UI): updated width oof questionHeader

--- components/QuestionText.jsx
+++ components/QuestionText.jsx
@@ -1,6 +1,6 @@
 export default function QuestionHeader({ questionText }) {
   return (
-    <div style={{ padding: '10px 20px 10px 20px' }}>
+    <div style={{ padding: '10px 20px 10px 20px' , width: '70%', fontSize: '20px'}}>
       {Array.isArray(questionText) ? (
         questionText.map((question, i) => <h3 key={i}>{question}</h3>)
       ) : (
