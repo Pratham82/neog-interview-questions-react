f4f01a9a458e89a4381f1022db0b28ecce23e98c
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-22 22:58:21 +0530

    feat(data): added question data

---
+++ data/conceptualQuestions.js
@@ -0,0 +1,25 @@
+const conceptualQuestion = [
+  {
+    question: 'What is DOM ?',
+    answer:
+      'The Document Object Model (DOM) is the data representation of the objects that comprise the structure and content of a document on the web. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page. JavaScript can access and change all the elements of an HTML document. When a web page is loaded, the browser creates a Document Object Model of the page.The  HTML DOM  model is constructed as a tree of Objects :',
+  },
+  {
+    question: 'What is an API ?',
+    answer:
+      'The acronym for Application Programming Interface.It is the medium by which software applications communicate with each other',
+  },
+
+  {
+    question: 'What is Semantic HTML ?',
+    answer:
+      'A semantic element clearly describes its meaning to both the browser and the developer. Examples of non-semantic elements: <div> and <span> - Tells nothing about its ',
+  },
+  {
+    question: 'Explain about promises  ',
+    answer:
+      'The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value. A Promise is in one of these states: pending initial state, neither fulfilled nor rejected. fulfilled meaning that the operation was completed successfully. rejected meaning that the operation failed',
+  },
+]
+
+export default conceptualQuestion
